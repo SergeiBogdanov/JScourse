2229c8cd48399992d7c36237c23d14546846fcad
Sergei <bsd1421@gmail.com>
2024-01-31 19:54:45 +0000

    Change1

--- script.js
+++ script.js
@@ -4,39 +4,5 @@ function showMessage() {
   let message = 'Привет, ' + userName;
   alert(message);
 }
-let userName = 'Вася';
-
-function showMessage() {
-  let message = 'Привет, ' + userName;
-  alert(message);
-}let userName = 'Вася';
-
-function showMessage() {
-  let message = 'Привет, ' + userName;
-  alert(message);
-}
 
 showMessage(); // Привет, Вася
-
-
-
-
-
-
-
-
-
-/*
-Замените код Function Expression стрелочной функцией:
-
- function ask(question, yes, no) {
-  if (confirm(question)) yes()
-  else no();
-}
-
-ask(
-  "Вы согласны?",
-  function() { alert("Вы согласились."); },
-  function() { alert("Вы отменили выполнение."); }
-);
-*/

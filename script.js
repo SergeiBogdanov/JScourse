f9c7e50825074aae61adffdf97128e3e99d91f2b
seerbos <21020386-seerbos@users.noreply.replit.com>
2024-01-31 19:14:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let userName = 'Вася';
+
+function showMessage() {
+  let message = 'Привет, ' + userName;
+  alert(message);
+}
+
+showMessage(); // Привет, Вася
+
+
+
+
+
+
+
+
+
+/*
+Замените код Function Expression стрелочной функцией:
+
+ function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Вы согласны?",
+  function() { alert("Вы согласились."); },
+  function() { alert("Вы отменили выполнение."); }
+);
+*/

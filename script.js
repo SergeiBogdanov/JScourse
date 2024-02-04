13573ddb68f39f592235dc90c057f92d79ef5d6c
SergeiBogdanov <130559217+SergeiBogdanov@users.noreply.github.com>
2024-02-04 13:48:47 +0000

    step1

---
+++ script.js
@@ -0,0 +1,17 @@
+/*let user = {
+  name: "John",
+  age: 30
+};
+
+user.surname = "Smith";
+
+alert(user.name + ' ' + user.surname);
+
+user.name = "Pete";
+
+alert(user.name + ' ' + user.surname);
+
+delete user.name;
+
+alert(user.name + ' ' + user.surname);*/
+

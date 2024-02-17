39ba8610290d603538067b43521bc9cccb974f6b
unknown <bsd1421@gmail.com>
2024-02-17 17:17:55 +0300

    Task4_2 is completed

--- Task4_2.js
+++ Task4_2.js
@@ -4,3 +4,15 @@ Using a loop, create an inverted version of an arbitrary string.
 For example, the line "Hello, world!" should turn into “!dlrow, olleH”.
 */
 
+/*Со строками можно работать так же, как и с массивами, 
+то есть получить i-й символ строки str можно с помощью str[i].
+Но в строках нельзя заменять i-й символ, то есть запись str[i] = 'x' ничего не изменит.*/
+
+let string = 'Hello World!';
+let result = '';
+
+for (let i = string.length - 1; i >= 0; i--) {
+    result += string[i];
+}
+
+console.log(result);

459beb67c138ba631f3bfcff6a2d5664e4892135
unknown <bsd1421@gmail.com>
2024-02-24 14:39:21 +0300

    Some changes

--- Task7_1/script.js
+++ Task7_1/script.js
@@ -1,3 +1,11 @@
+/*
+Exersise:
+Create a web page that allows users to increment a counter value by one. 
+Use HTML to place interface elements, such as the <p> element 
+to display the current counter value and the <button> button to increment the counter. 
+Then add JavaScript to add functionality that increments the counter value by one when the button is clicked.
+*/
+
 let countDisplay = document.querySelector(".count-display");
 let incrementButton = document.querySelector(".icrement-button");
 
--- Task7_2/script.js
+++ Task7_2/script.js
@@ -1,3 +1,11 @@
+/*
+Exersise: 
+Create a page that allows users to select a color and fill a block with that color. 
+Use HTML to place controls such as <input> for color selection and <button> for clearing. 
+Then add JavaScript to add interactivity, such as changing the color of the block 
+when you select a color and clearing the block when you click the "Clear" button.
+*/
+
 document.addEventListener('DOMContentLoaded', function() {
     let colorInput = document.querySelector('.color-input');
     let colorBlock = document.querySelector('.color-block');
